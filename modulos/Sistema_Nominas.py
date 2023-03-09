d0489e71de5fc34de22ed915982b3311368b18c2
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-03-09 16:51:05 +0000

    cambios 465

--- modulos/Sistema_Nominas.py
+++ modulos/Sistema_Nominas.py
@@ -2,9 +2,12 @@ class Sistema_Nominas:
     def calcular_nominas(self,empleados):
         print("Calculando nominas")
         print("==============================================")
+      
         for emp in empleados:
+             #si el empleado es analista imprimo la metodologia y el nombre
             if emp.puesto=="analista":
                 print(f"Nomina para: {emp.nombre} - {emp.metodologia}")
+            #si el empleado es programador imprimo nombre y lenguaje de programacion
             elif emp.puesto=="programador":
                  print(f"Nomina para: {emp.nombre} - {emp.lenguaje_programacion}")
 

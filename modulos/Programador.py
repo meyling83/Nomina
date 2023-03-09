54c04c08307bcf61dc2d5c2f566d740698dd375c
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-03-09 14:11:22 +0000

    empleados1

--- modulos/Programador.py
+++ modulos/Programador.py
@@ -4,4 +4,9 @@ class Programador(Empleado):
         super().__init__(nombre,salario)
         self.lenguaje_programacion=lenguaje_programacion
     def calcular_salario(self):
-        pass
+        if lenguaje_programacion.lower()=="python":
+            return self.salario*1.5
+        elif lenguaje_programacion.lower=="java":
+            return self.salario*1.3
+        else:
+            return self.salario*1.2

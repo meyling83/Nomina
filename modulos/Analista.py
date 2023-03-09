e57d5d5fd8561d80bd1b8706c9612ba4f4fe9f7e
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-03-09 16:49:19 +0000

    cambio5699

--- modulos/Analista.py
+++ modulos/Analista.py
@@ -1,7 +1,9 @@
+#clase analista, hereda de Empleado
 from modulos.Empleado import Empleado
 class Analista(Empleado):
     def __init__(self,nombre,salario,puesto,metodologia):
         super().__init__(nombre,salario,puesto)
         self.metodologia=metodologia
+    #funcion que calcula el salario del analista multiplicando por 1.8
     def calcular_salario(self):
         return self.salario*1.8

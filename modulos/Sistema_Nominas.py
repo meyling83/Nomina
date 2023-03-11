5adb7e75d105dd18e3f626e05812a8fd7b660f65
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-03-11 14:48:46 +0000

    cambio final

--- modulos/Sistema_Nominas.py
+++ modulos/Sistema_Nominas.py
@@ -6,11 +6,12 @@ class Sistema_Nominas:
         for emp in empleados:
              #si el empleado es analista imprimo la metodologia y el nombre
             if emp.puesto=="analista":
-                print(f"Nomina para: {emp.nombre} - {emp.metodologia}")
+                print(f"Nomina para: {emp.nombre} -  {emp.puesto} - {emp.metodologia}")
             #si el empleado es programador imprimo nombre y lenguaje de programacion
             elif emp.puesto=="programador":
-                 print(f"Nomina para: {emp.nombre} - {emp.lenguaje_programacion}")
-
+                print(f"Nomina para: {emp.nombre} -  {emp.puesto} - {emp.lenguaje_programacion}")
+            else:
+                print(f"Nomina para: {emp.nombre} - {emp.puesto}")
             print(f"-$:{emp.calcular_salario()}")
             print("")
 
